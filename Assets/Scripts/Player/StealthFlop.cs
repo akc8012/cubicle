8ef041a7836214ba4efc864b6ebb772bc2fea20a
Andrew Colannino <akcolannino@email.neit.edu>
2016-08-19 19:17:29 -0400

    Added stealth man

---
+++ Assets/Scripts/Player/StealthFlop.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StealthFlop : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Stealth/LookAtPlayer.cs
@@ -0,0 +1,29 @@
+﻿// Andrew
+using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour
+{
+	public float rayY = 0.6f;
+
+	//Transform player;
+
+	void Start()
+	{
+		//player = GameObject.FindWithTag("Player").transform;
+	}
+
+	void Update()
+	{
+		//transform.Rotate(new Vector3(0, 1, 0));
+
+		Vector3 rayLoc = transform.position;
+		rayLoc.y += rayY;
+
+		Ray ray = new Ray(rayLoc, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit))
+			Debug.DrawLine(ray.origin, hit.point);
+	}
+}
