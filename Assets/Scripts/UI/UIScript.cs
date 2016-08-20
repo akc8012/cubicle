f65b1ec57867f7e4b86eb719c72424e035dfab29
ThatDudeMike <mgagne002@gmail.com>
2016-08-20 08:52:47 -0400

    Added Sounds and added UI.

---
+++ Assets/Scripts/UI/UIScript.cs
@@ -0,0 +1,26 @@
+﻿//MICHAEL
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public int floorCounter;
+
+    public Text text;
+	void Start()
+	{
+        floorCounter = 1;
+        text.text = "Floor Count: " + floorCounter;
+	}
+
+	void Update()
+	{
+        text.text = "Floor Count:  " + floorCounter;
+	}
+
+    public void CountUp()
+    {
+        floorCounter++;
+    }
+}
