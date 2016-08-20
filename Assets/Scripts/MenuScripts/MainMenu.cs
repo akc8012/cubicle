6e9732122e774c2b4c8bbb1ed20f80e76a7dbee0
ThatDudeMike <mgagne002@gmail.com>
2016-08-20 06:57:57 -0400

    ButtonSoundsAndPewpew

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+
+	void Start()
+	{
+
+	}
+
+    void Update()
+	{
+
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuScripts/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayButton : MonoBehaviour, ISelectHandler
+{
+    AudioSource audio;
+
+	void Start()
+	{
+        audio = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		
+	}
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        audio.Play();
+    }
+}
