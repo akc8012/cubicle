892568bab73d29747358c96ee76a6536463161a9
ThatDudeMike <mgagne002@gmail.com>
2016-08-19 22:33:29 -0400

    NerfExplosions Added

---
+++ Assets/Scripts/NerfThings/NerfBullet.cs
@@ -0,0 +1,24 @@
+﻿//MICHAEL
+using UnityEngine;
+using System.Collections;
+
+public class NerfBullet : MonoBehaviour
+{
+    public GameObject explosionParticle;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Instantiate(explosionParticle, this.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/NerfThings/NerfParticle.cs
@@ -0,0 +1,20 @@
+﻿//MICHAEL
+using UnityEngine;
+using System.Collections;
+
+public class NerfParticle : MonoBehaviour
+{
+    public float lifeTimer;
+
+	void Start()
+	{
+        lifeTimer = .5f;
+	}
+
+	void Update()
+	{
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0)
+            Destroy(gameObject);
+    }
+}
