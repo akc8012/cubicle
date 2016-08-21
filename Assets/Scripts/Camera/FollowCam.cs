7936e94ba11458e15a983fb7e8d088347a04f912
Andrew Colannino <akcolannino@email.neit.edu>
2016-08-21 14:30:10 -0400

    Camera Tweaks

--- Assets/Scripts/Camera/FollowCam.cs
+++ Assets/Scripts/Camera/FollowCam.cs
@@ -6,25 +6,49 @@ public class FollowCam : MonoBehaviour
 {
 	public float dist = 1;
 	public float vert = 1;
+	public Transform camStopA;
+	public Transform camStopB;
 
 	Transform player;
 	float startY;
-	float startX;
+	//float startX;
 
 	void Start()
 	{
 		player = GameObject.FindWithTag("Player").transform;
 		startY = player.position.y;
-		startX = transform.position.x;
+		//startX = transform.position.x;
+
+		Vector3 target = player.position;
+		Vector3 newPos;
+		//target.x = startX;
+		target.y = startY;
+
+		newPos = target - Vector3.forward * dist;
+		newPos += Vector3.up * vert;
+
+		transform.position = newPos;
 	}
 
 	void LateUpdate()
 	{
 		Vector3 target = player.position;
-		target.x = startX;
+		Vector3 newPos;
+		//target.x = startX;
 		target.y = startY;
 
-		transform.position = target - Vector3.forward * dist;
-		transform.position += Vector3.up * vert;
+		newPos = target - Vector3.forward * dist;
+		newPos += Vector3.up * vert;
+
+		if (newPos.x != transform.position.x)
+		{
+			if (newPos.x < transform.position.x && newPos.x > camStopA.position.x ||
+			newPos.x > transform.position.x && newPos.x < camStopB.position.x)
+				transform.position = newPos;
+			else
+				transform.position = new Vector3(transform.position.x, newPos.y, newPos.z);
+		}
+		else
+			transform.position = newPos;
 	}
 }
